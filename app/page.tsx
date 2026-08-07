a438c7e65c168d02412f0c3f092d63cb79ba7030
Grisham Prateesh <grishamprateesh7@gmail.com>
2026-08-07 23:17:48 +0530

    refactor: clean up non-project files and finalize light mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-slate-50 dark:bg-slate-950">
+    <div className="flex flex-col w-full">
 
       {/* HERO SECTION */}
       <section className="relative w-full pb-32 pt-20 lg:pt-32 overflow-hidden flex items-center justify-center">
